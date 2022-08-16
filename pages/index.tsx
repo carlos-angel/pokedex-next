f9c0bce3e2fe399c51e717041b1943656af2dafa
carlos alberto angel angel <c.a.a.a.503@gmail.com>
2022-08-15 23:44:42 -0500

    build: instalar y configurar NextUI

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,11 @@
 import type { NextPage } from 'next';
+import { Button } from '@nextui-org/react';
 
 const Home: NextPage = () => {
   return (
     <>
       <h1>Hello world!</h1>
+      <Button color='gradient'>Click me</Button>
     </>
   );
 };
