40b396c8b1649d30ba32ca3eb51e2f9cbdf69805
carlos alberto angel angel <c.a.a.a.503@gmail.com>
2022-08-15 23:55:56 -0500

    feat: agregar dark mode

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,11 @@
 import '../styles/globals.css';
 import type { AppProps } from 'next/app';
 import { NextUIProvider } from '@nextui-org/react';
+import { darkTheme } from '../themes';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <NextUIProvider>
+    <NextUIProvider theme={darkTheme}>
       <Component {...pageProps} />
     </NextUIProvider>
   );
