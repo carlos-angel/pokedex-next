176c3b844ca4701e2c51c485e6269a0eca46fb39
carlos alberto angel angel <c.a.a.a.503@gmail.com>
2022-08-19 23:29:03 -0500

    feat: cargar de forma estática los pokemons

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,44 @@
 import type { NextPage } from 'next';
-import { Button } from '@nextui-org/react';
+import { GetStaticProps } from 'next';
 import { Layout } from '../components/layouts';
+import { pokeApi } from '../api';
+import { PokemonListResponse, SmallPokemon } from '../interfaces';
 
-const Home: NextPage = () => {
+interface Props {
+  pokemons: SmallPokemon[];
+}
+
+const Home: NextPage<Props> = ({ pokemons }) => {
   return (
     <Layout title='Pokedex'>
-      <h1>Hello world!</h1>
-      <Button color='gradient'>Click me</Button>
+      <h1>Pokemons</h1>
+      <ul>
+        {pokemons.map((pokemon) => (
+          <li key={pokemon.id}>
+            # {pokemon.id} {pokemon.name}
+          </li>
+        ))}
+      </ul>
     </Layout>
   );
+};
+
+export const getStaticProps: GetStaticProps = async (ctx) => {
+  const { data } = await pokeApi.get<PokemonListResponse>('pokemon?limit=151');
+
+  const pokemons: SmallPokemon[] = data.results.map((pokemon, i) => ({
+    ...pokemon,
+    id: i + 1,
+    img: `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/${
+      i + 1
+    }.svg`,
+  }));
+
+  return {
+    props: {
+      pokemons,
+    },
+  };
 };
 
 export default Home;
