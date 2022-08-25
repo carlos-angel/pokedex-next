1254cb9e2650dd621f1c29280fa34f00ba82f54f
carlos alberto angel angel <c.a.a.a.503@gmail.com>
2022-08-24 21:13:57 -0500

    feat: agregar página para un pokemon por nombre

---
+++ pages/name/[name].tsx
@@ -0,0 +1,127 @@
+import { useState, useEffect } from 'react';
+import { NextPage } from 'next';
+import { GetStaticPaths, GetStaticProps } from 'next';
+import { Pokemon, PokemonListResponse } from '../../interfaces';
+import { pokeApi } from '../../api';
+import { Layout } from '../../components/layouts';
+import { existInFavorites, toggleFavorite } from '../../utils';
+import { Grid, Card, Text, Button, Container, Image } from '@nextui-org/react';
+import confetti from 'canvas-confetti';
+
+interface Props {
+  pokemon: Pokemon;
+}
+
+const PokemonByNamePage: NextPage<Props> = ({ pokemon }) => {
+  const [isInFavorites, setIsInFavorites] = useState<boolean>(false);
+
+  const onToggleFavorite = () => {
+    toggleFavorite(pokemon.id);
+    setIsInFavorites((prev) => !prev);
+    if (!isInFavorites) {
+      confetti({
+        zIndex: 999,
+        particleCount: 100,
+        spread: 160,
+        angle: -100,
+        origin: {
+          x: 1,
+          y: 0,
+        },
+      });
+    }
+  };
+
+  useEffect(() => {
+    const isFav = existInFavorites(pokemon.id);
+    setIsInFavorites(isFav);
+  }, [pokemon.id]);
+
+  return (
+    <Layout title={`Pokemon ${pokemon.name} `}>
+      <Grid.Container css={{ marginTop: '5px' }} gap={2}>
+        <Grid xs={12} sm={4}>
+          <Card isHoverable css={{ padding: '30px' }}>
+            <Card.Body>
+              <Card.Image
+                src={pokemon.sprites.other?.dream_world.front_default || '/no-image.png'}
+                alt={pokemon.name}
+                width='100%'
+                height={200}
+              />
+            </Card.Body>
+          </Card>
+        </Grid>
+
+        <Grid xs={12} sm={8}>
+          <Card>
+            <Card.Header css={{ display: 'flex', justifyContent: 'space-between' }}>
+              <Text h1 transform='capitalize'>
+                {pokemon.name}
+              </Text>
+
+              <Button color='gradient' ghost={!isInFavorites} onClick={onToggleFavorite}>
+                {isInFavorites ? 'En favoritos' : 'Guardar en favoritos'}
+              </Button>
+            </Card.Header>
+
+            <Card.Body>
+              <Text size={30}>Sprites:</Text>
+              <Container direction='row' display='flex' gap={0}>
+                <Image
+                  src={pokemon.sprites.front_default}
+                  alt={pokemon.name}
+                  width={100}
+                  height={100}
+                />
+                <Image
+                  src={pokemon.sprites.back_default}
+                  alt={pokemon.name}
+                  width={100}
+                  height={100}
+                />
+                <Image
+                  src={pokemon.sprites.front_shiny}
+                  alt={pokemon.name}
+                  width={100}
+                  height={100}
+                />
+                <Image
+                  src={pokemon.sprites.back_shiny}
+                  alt={pokemon.name}
+                  width={100}
+                  height={100}
+                />
+              </Container>
+            </Card.Body>
+          </Card>
+        </Grid>
+      </Grid.Container>
+    </Layout>
+  );
+};
+
+export const getStaticPaths: GetStaticPaths = async (ctx) => {
+  const { data } = await pokeApi.get<PokemonListResponse>('pokemon?limit=151');
+
+  const paths = data.results.map(({ name }) => ({ params: { name } }));
+
+  return {
+    paths,
+    fallback: false,
+  };
+};
+
+export const getStaticProps: GetStaticProps = async (ctx) => {
+  const { name } = ctx.params as { name: string };
+
+  const { data } = await pokeApi.get<Pokemon>(`pokemon/${name}`);
+
+  return {
+    props: {
+      pokemon: data,
+    },
+  };
+};
+
+export default PokemonByNamePage;
