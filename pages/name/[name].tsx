77d6edb550a81777dfe8d58230970df056cf9b02
carlos alberto angel angel <c.a.a.a.503@gmail.com>
2022-08-25 20:26:49 -0500

    perf: aplicar ISR y ISG en la página PokemonByNamePage

--- pages/name/[name].tsx
+++ pages/name/[name].tsx
@@ -108,13 +108,22 @@ export const getStaticPaths: GetStaticPaths = async (ctx) => {
 
   return {
     paths,
-    fallback: false,
+    fallback: 'blocking',
   };
 };
 
 export const getStaticProps: GetStaticProps = async (ctx) => {
   const { name } = ctx.params as { name: string };
   const pokemon = await getPokemonInfo(name);
+
+  if (!pokemon) {
+    return {
+      redirect: {
+        destination: '/',
+        permanent: false,
+      },
+    };
+  }
 
   return {
     props: {
